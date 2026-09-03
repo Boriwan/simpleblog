c81e4919306bf9b392b13050e797491b1bcc8369
Boriwan <43809289+Boriwan@users.noreply.github.com>
2026-09-03 15:27:47 +0200

    Added a new Article route and controller

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
